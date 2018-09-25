c9403dd2f927f478543ee52a2722c4c083f579b1
yanan <yananzx@yeah.net>
2018-09-26 05:37:06 +0800

    阅读列表项目开发

---
+++ readinglist/src/main/java/com/example/readinglist/Book.java
@@ -0,0 +1,56 @@
+package com.example.readinglist;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="book")
+public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="isbn")
+	private String isbn;
+	
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="author")
+	private String author;
+	
+	@Column(name="description")
+	private String description;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	} }
